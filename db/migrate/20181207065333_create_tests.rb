e10eb96e342bc48a1edbc4ff7dea49b182cc4960
Viktor Kachulin <vicart2008@gmail.com>
2018-12-09 22:43:47 +1000

    Remove attributes string limit, removed reference attributes to properly add it later

--- db/migrate/20181207065333_create_tests.rb
+++ db/migrate/20181207065333_create_tests.rb
@@ -1,9 +1,8 @@
 class CreateTests < ActiveRecord::Migration[5.2]
   def change
     create_table :tests do |t|
-      t.string :title, limit: 50
+      t.string :title
       t.integer :level
-      t.integer :category_id
 
       t.timestamps
     end
