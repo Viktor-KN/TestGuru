c59db4445a5a33cee7eafc7fbee29b45a918d7a7
Viktor Kachulin <vicart2008@gmail.com>
2018-12-11 13:26:49 +1000

    Add User#completed_tests_by_level method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
   has_many :results
   has_many :tests, through: :results
+
+  def completed_tests_by_level(level)
+    tests.select { |test| test.level == level }
+  end
 end
