ef6cad5808069348e19ff0721878c471116f7841
Viktor Kachulin <vicart2008@gmail.com>
2018-12-11 13:27:52 +1000

    Add Test.titles_by_category class method

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,8 @@
 class Test < ApplicationRecord
   belongs_to :category
   has_many :questions
+
+  def self.titles_by_category(cat_title)
+    joins(:category).where('categories.title' => cat_title).distinct.order('tests.id DESC').pluck(:title)
+  end
 end
