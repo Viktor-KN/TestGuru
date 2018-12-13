8115638e6649346b34376b6d0b2842fa5b50b9b0
Viktor Kachulin <vicart2008@gmail.com>
2018-12-13 12:08:08 +1000

    More refactoring

--- app/models/test.rb
+++ app/models/test.rb
@@ -3,6 +3,6 @@ class Test < ApplicationRecord
   has_many :questions
 
   def self.titles_by_category(category_title)
-    joins(:category).where(categories: { title: category_title }).distinct.order('tests.title DESC').pluck(:title)
+    joins(:category).where(categories: { title: category_title }).distinct.order(title: :desc).pluck(:title)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,40 +6,35 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-QUESTIONS_PER_TEST = 2
-MIN_COMPLETED_TESTS = 2
-RANDOM_SEED = 777 # Keeps generated random numbers persistent between db reseeding
-category_titles = %w[Ruby SQL Rails]
-user_names = %w[Bill Bob Alice]
-categories = []
-users = []
-tests = []
-questions = []
-rnd = Random.new(RANDOM_SEED)
+questions_per_test = 2
+min_completed_tests = 2
+random_seed = 777 # Keeps generated random numbers persistent between db reseeding
+categories = %w[Ruby SQL Rails]
+users = %w[Bill Bob Alice]
+rnd = Random.new(random_seed)
 
+categories.map! { |category| Category.create!(title: category) }
+users.map! { |user| User.create!(name: user) }
 
-category_titles.each { |category| categories << Category.create!(title: category) }
-user_names.each { |user| users << User.create!(name: user) }
-
-categories.each do |category|
-  tests << Test.create!(title: "#{category.title} Basics", level: 0, category_id: category.id)
-  tests << Test.create!(title: "Advanced #{category.title}", level: 1, category_id: category.id)
+tests = categories.flat_map do |category|
+  Test.create!([{ title: "#{category.title} Basics", level: 0, category: category },
+                { title: "Advanced #{category.title}", level: 1, category: category }])
 end
 
-tests.each do |test|
-  (1..QUESTIONS_PER_TEST).each do |i|
-    questions << Question.create!(body: "Question #{test.title} #{i}", test_id: test.id)
+questions = tests.flat_map do |test|
+  Array.new(questions_per_test) do |i|
+    Question.create!(body: "Question #{test.title} #{i + 1}", test: test)
   end
 end
 
 questions.each do |question|
-  Answer.create!(body: "Correct answer to #{question.body}", correct: true, question_id: question.id)
-  Answer.create!(body: "Incorrect answer to #{question.body}", correct: false, question_id: question.id)
+  Answer.create!([{ body: "Correct answer to #{question.body}", correct: true, question: question },
+                  { body: "Incorrect answer to #{question.body}", correct: false, question: question }])
 end
 
 tests_count = tests.size
 users.each do |user|
-  rnd.rand(MIN_COMPLETED_TESTS..tests_count).times do
-    Result.create!(user_id: user.id, test_id: tests[rnd.rand(tests_count)].id)
+  rnd.rand(min_completed_tests..tests_count).times do
+    Result.create!(user: user, test: tests[rnd.rand(tests_count)])
   end
 end
