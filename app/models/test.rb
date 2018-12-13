cef22226d1f8bfdcd2479cad34cadcf52ce1a783
Viktor Kachulin <vicart2008@gmail.com>
2018-12-13 18:43:28 +1000

    Define new and fix older model associations

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,9 @@
 class Test < ApplicationRecord
   belongs_to :category
-  has_many :questions
+  belongs_to :author, class_name: 'User'
+  has_many :results, dependent: :destroy
+  has_many :participants, through: :results, source: :user
+  has_many :questions, dependent: :destroy
 
   def self.titles_by_category(category_title)
     joins(:category).where(categories: { title: category_title }).distinct.order(title: :desc).pluck(:title)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
-  has_many :results
-  has_many :tests, through: :results
+  has_many :results, dependent: :destroy
+  has_many :completed_tests, through: :results, source: :test
+  has_many :created_tests, class_name: 'Test', foreign_key: 'author_id', dependent: :nullify
 
   def completed_tests_by_level(level)
-    tests.where(level: level)
+    completed_tests.where(level: level)
   end
 end
