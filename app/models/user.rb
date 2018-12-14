b0dd545873f912b8979464135097e67f41f1eeb9
Viktor Kachulin <vicart2008@gmail.com>
2018-12-14 14:57:00 +1000

    Add scope methods to models

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,4 @@ class User < ApplicationRecord
   has_many :results, dependent: :destroy
   has_many :completed_tests, through: :results, source: :test
   has_many :created_tests, class_name: 'Test', foreign_key: 'author_id', dependent: :nullify
-
-  def completed_tests_by_level(level)
-    completed_tests.where(level: level)
-  end
 end
