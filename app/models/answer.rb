162fb421b08e8063e08408ba0472a3d06bc664c1
Viktor Kachulin <vicart2008@gmail.com>
2018-12-15 23:12:00 +1000

    Fix models validations and scope methods

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -4,14 +4,14 @@ class Answer < ApplicationRecord
   belongs_to :question
 
   validates :body, presence: true
-  validate :validate_max_answers_per_question
+  validate :validate_max_answers_per_question, on: :create
 
   scope :correct, -> { where(correct: true) }
 
   private
 
   def validate_max_answers_per_question
-    if Answer.where(question_id: question_id).count >= MAX_ANSWERS_PER_QUESTION
+    if question.answers.count >= MAX_ANSWERS_PER_QUESTION
       errors.add(:question_id, "Maximum answers per question reached")
     end
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,6 @@ class User < ApplicationRecord
   has_many :completed_tests, through: :results, source: :test
   has_many :created_tests, class_name: 'Test', foreign_key: 'author_id', dependent: :nullify
 
-  validates :name, presence: true
-  validates :email, presence: true
-  validates :name, uniqueness: true
+  validates :name, :email, presence: true
   validates :email, uniqueness: true
 end
