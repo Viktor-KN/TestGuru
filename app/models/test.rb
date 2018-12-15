549e9d4ecf61e59bd5d4acb5b8d3a137a1dbe49c
Viktor Kachulin <vicart2008@gmail.com>
2018-12-15 23:12:00 +1000

    Fix models validations and scope methods

--- app/models/test.rb
+++ app/models/test.rb
@@ -5,14 +5,16 @@ class Test < ApplicationRecord
   has_many :participants, through: :results, source: :user
   has_many :questions, dependent: :destroy
 
-  validates :title, presence: true
+  validates :title, presence: true, uniqueness: { scope: :level }
   validates :level, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :title, uniqueness: { scope: :level }
 
-  scope :easy, -> { where(level: 0..1) }
-  scope :middle, -> { where(level: 3..4) }
-  scope :hard, -> { where(level: 5..Float::INFINITY) }
   scope :by_category, ->(category_title) { joins(:category).where(categories: { title: category_title }) }
-  scope :titles_by_category, ->(category_title) { by_category(category_title).distinct.order(title: :desc).pluck(:title) }
   scope :by_level, ->(level) { where(level: level) }
+  scope :easy, -> { by_level(0..1) }
+  scope :middle, -> { by_level(2..4) }
+  scope :hard, -> { by_level(5..Float::INFINITY) }
+
+  def self.titles_by_category(category_title)
+    by_category(category_title).distinct.order(title: :desc).pluck(:title)
+  end
 end
