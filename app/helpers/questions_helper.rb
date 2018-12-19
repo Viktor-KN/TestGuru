06b7d60f5a8981394c43f1b397c161047cdeed89
Viktor Kachulin <vicart2008@gmail.com>
2018-12-19 16:54:06 +1000

    Add new helpers

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,7 @@
 module QuestionsHelper
+  QUESTION_ACTIONS = { new: 'Create new', edit: 'Edit' }.freeze
+
+  def form_header(action, test)
+    "#{QUESTION_ACTIONS[action]} #{test.title} Question"
+  end
 end
