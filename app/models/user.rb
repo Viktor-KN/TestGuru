15704380bf59e8238578a537a22fc7cc0358d2ed
Viktor Kachulin <vicart2008@gmail.com>
2018-12-24 00:26:55 +1000

    Change models associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  has_many :results, dependent: :destroy
-  has_many :completed_tests, through: :results, source: :test
-  has_many :created_tests, class_name: 'Test', foreign_key: 'author_id', dependent: :nullify
+  has_many :test_passages, dependent: :destroy
+  has_many :tests, through: :test_passages, source: :test
+  has_many :authored_tests, class_name: 'Test', foreign_key: 'author_id', dependent: :nullify
 
   validates :name, :email, presence: true
   validates :email, uniqueness: true
