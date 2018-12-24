edd6be024f2766c8a07114974f8f8fbf957703a5
Viktor Kachulin <vicart2008@gmail.com>
2018-12-24 14:42:01 +1000

    Add test_passage method, fix tests association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ApplicationRecord
   has_many :test_passages, dependent: :destroy
-  has_many :tests, through: :test_passages, source: :test
+  has_many :tests, through: :test_passages
   has_many :authored_tests, class_name: 'Test', foreign_key: 'author_id', dependent: :nullify
 
   validates :name, :email, presence: true
   validates :email, uniqueness: true
+
+  def test_passage(test)
+    test_passages.order(id: :desc).find_by(test_id: test.id)
+  end
 end
