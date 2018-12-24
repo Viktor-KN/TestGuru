6c4b092b87718b333692bc88e1bb88349f57590f
Viktor Kachulin <vicart2008@gmail.com>
2018-12-24 21:50:00 +1000

    Add set_next_question callback. Add current_question_index method. Fix correct_answer? method.

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -4,11 +4,10 @@ class TestPassage < ApplicationRecord
   belongs_to :current_question, class_name: 'Question', optional: true
 
   before_validation :before_validation_set_first_question, on: :create
+  before_update :before_update_set_next_question
 
   def accept!(answer_ids)
     self.correct_questions += 1 if correct_answer?(answer_ids)
-
-    self.current_question = next_question
     save!
   end
 
@@ -16,9 +15,14 @@ class TestPassage < ApplicationRecord
     current_question.nil?
   end
 
+  def current_question_index
+    test.questions.order(:id).ids.find_index(current_question.id) + 1
+  end
+
   private
 
   def correct_answer?(answer_ids)
+    answer_ids ||= []
     correct_answers.ids.sort == answer_ids.map(&:to_i).sort
   end
 
@@ -32,5 +36,9 @@ class TestPassage < ApplicationRecord
 
   def before_validation_set_first_question
     self.current_question = test.questions.first if test.present?
+  end
+
+  def before_update_set_next_question
+    self.current_question = next_question
   end
 end
