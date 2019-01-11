ee30f05111b7489c485b66f5d6ac065b3aa78dda
Viktor Kachulin <vicart2008@gmail.com>
2019-01-11 10:56:02 +1000

    Replace @user with current_user in Tests Controller

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,7 +1,6 @@
 class TestsController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[index]
   before_action :find_test, only: %i[show edit update destroy start]
-  before_action :set_user, only: %i[create start]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_resource_not_found
 
@@ -16,7 +15,7 @@ class TestsController < ApplicationController
   end
 
   def create
-    @test = @user.authored_tests.new(test_params)
+    @test = current_user.authored_tests.new(test_params)
     if @test.save
       redirect_to @test
     else
@@ -40,18 +39,14 @@ class TestsController < ApplicationController
   end
 
   def start
-    @user.tests.push(@test)
-    redirect_to @user.test_passage(@test)
+    current_user.tests.push(@test)
+    redirect_to current_user.test_passage(@test)
   end
 
   private
 
   def find_test
     @test = Test.find(params[:id])
-  end
-
-  def set_user
-    @user = current_user
   end
 
   def test_params
