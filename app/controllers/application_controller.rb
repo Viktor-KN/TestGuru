06c56fdf52d4972a2ce5596c0ed9c9de7b5881fd
Viktor Kachulin <vicart2008@gmail.com>
2019-01-14 16:20:03 +1000

    Customize registrations view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password) }
+  end
 
   def rescue_with_resource_not_found(exception)
     render inline: "<h2>#{exception.message}</h2>"
