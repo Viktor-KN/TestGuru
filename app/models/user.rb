fe8c199d6f4608f8cc29a07d8680fdcab74eb312
Viktor Kachulin <vicart2008@gmail.com>
2019-01-14 13:20:00 +1000

    Move email validation regex to const

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ApplicationRecord
+  EMAIL_REGEX = /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/.freeze
+
   has_many :test_passages, dependent: :destroy
   has_many :tests, through: :test_passages
   has_many :authored_tests, class_name: 'Test', foreign_key: 'author_id', dependent: :nullify
 
   validates :name, presence: true, length: { minimum: 3, maximum: 64 }
-  validates :email, presence: true, uniqueness: true, format: /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
+  validates :email, presence: true, uniqueness: true, format: EMAIL_REGEX
 
   has_secure_password
 
