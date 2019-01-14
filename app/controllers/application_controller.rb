cb8444806ea674452161a3d795b07e4697c67279
Viktor Kachulin <vicart2008@gmail.com>
2019-01-15 01:38:54 +1000

    Fix new user registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password) }
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password, :password_confirmation) }
   end
 
   def user_is_an_admin?
