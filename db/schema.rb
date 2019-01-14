26672f8ec1593f0b4449110c829862a4cd881e08
Viktor Kachulin <vicart2008@gmail.com>
2019-01-14 18:00:32 +1000

    Add Admin model iherited from User model. Fix old attributes usage

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_14_034117) do
+ActiveRecord::Schema.define(version: 2019_01_14_073416) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
@@ -60,8 +60,6 @@ ActiveRecord::Schema.define(version: 2019_01_14_034117) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name", null: false
-    t.boolean "admin", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
@@ -78,6 +76,9 @@ ActiveRecord::Schema.define(version: 2019_01_14_034117) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
+    t.string "type", default: "User", null: false
+    t.string "first_name", default: "", null: false
+    t.string "last_name", default: "", null: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
