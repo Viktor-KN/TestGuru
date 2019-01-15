04d57844be6118ca6925342c2bc284260b117890
Viktor Kachulin <vicart2008@gmail.com>
2019-01-15 10:30:02 +1000

    Refactor

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,7 @@ class User < ApplicationRecord
   has_many :tests, through: :test_passages
   has_many :authored_tests, class_name: 'Test', foreign_key: 'author_id', dependent: :nullify
 
-  validates :first_name, :last_name, presence: true, length: { minimum: 3, maximum: 64 }
-  validates :email, presence: true, uniqueness: true
+  validates :first_name, :last_name, presence: true, length: 3..64
 
   def test_passage(test)
     test_passages.order(id: :desc).find_by(test: test)
