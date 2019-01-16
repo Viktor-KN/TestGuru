5714008faed0e857664c0e9492fc9a82da4b6890
Viktor Kachulin <vicart2008@gmail.com>
2019-01-16 17:18:39 +1000

    Implement internatiolization with ru and en languages

--- app/helpers/answers_helper.rb
+++ app/helpers/answers_helper.rb
@@ -1,6 +1,6 @@
 module AnswersHelper
   def answer_form_header(answer)
-    action = answer.new_record? ? 'Create new' : 'Edit'
-    "#{action} answer for question: #{answer.question.body}"
+    action = answer.new_record? ? I18n.t('shared.create_new') : I18n.t('shared.edit')
+    I18n.t('shared.helpers.answer_form_header', action: action, question_body: answer.question.body)
   end
 end
--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,6 +1,6 @@
 module QuestionsHelper
   def question_form_header(question)
-    action = question.new_record? ? 'Create new' : 'Edit'
-    "#{action} #{question.test.title} Question"
+    action = question.new_record? ? I18n.t('shared.create_new') : I18n.t('shared.edit')
+    I18n.t('shared.helpers.question_form_header', action: action, test_title: question.test.title)
   end
 end
