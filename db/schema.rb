047078e2c56b1c72a41b988041b2d765a5a2dec3
Viktor Kachulin <vicart2008@gmail.com>
2019-01-29 17:41:27 +1000

    Move to postgresql db

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_01_21_134233) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
     t.boolean "correct", default: false, null: false
