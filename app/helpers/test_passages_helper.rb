2c5de7dc85f29d33cc7b306ff3b1c371d9728d48
Viktor Kachulin <vicart2008@gmail.com>
2019-02-12 17:57:35 +1000

    Implement test passage time counter

--- app/helpers/test_passages_helper.rb
+++ app/helpers/test_passages_helper.rb
@@ -6,4 +6,20 @@ module TestPassagesHelper
   def test_result_status(test_passage)
     test_passage.result_success? ? I18n.t('test_passages.result.passed') : I18n.t('test_passages.result.failed')
   end
+
+  def seconds_left(test_passage)
+    ((test_passage.created_at + test_passage.test.time_limit.minutes) - Time.current).to_i
+  end
+
+  def time_counter(test_passage)
+    total_seconds = seconds_left(test_passage)
+
+    return '00:00:00' if total_seconds.negative?
+
+    seconds = total_seconds % 60
+    minutes = (total_seconds / 60) % 60
+    hours = total_seconds / (60 * 60)
+
+    format("%02d:%02d:%02d", hours, minutes, seconds)
+  end
 end
